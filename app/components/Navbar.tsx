87d6d649cf98ea2a81229289438fbd682aeff02d
srijan mukherjee <emailofsrijan@gmail.com>
2022-11-05 23:37:41 +0530

    Sticky nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { FC, PropsWithChildren } from "react";
 import CustomWalletButton from "./nossr/CustomWalletButton";
 import NavbarLink from "./NavbarLink";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faGhost, faHashtag } from "@fortawesome/free-solid-svg-icons";
 
 interface NavbarProps extends PropsWithChildren {}
 
@@ -16,14 +16,20 @@ const useStyles = createStyles((theme) => ({
 	},
 }));
 
-const navbarLinks = [{ label: "Explore", icon: <FontAwesomeIcon icon={faCoffee} /> }];
+const navbarLinks = [
+	{ label: "Explore", href: "/", icon: faHashtag },
+	{ label: "Creators", href: "/creators", icon: faGhost },
+];
 
 const Navbar: FC<NavbarProps> = () => {
 	const { classes } = useStyles();
 	const links = navbarLinks.map((item) => <NavbarLink {...item} key={item.label} />);
 
 	return (
-		<MantineNavbar width={{ base: "100%" }} height="100%" style={{ background: "transparent", border: "none" }}>
+		<MantineNavbar
+			width={{ base: "100%" }}
+			height="100%"
+			style={{ background: "transparent", border: "none", padding: 8 }}>
 			<MantineNavbar.Section grow>{links}</MantineNavbar.Section>
 			<MantineNavbar.Section>
 				<CustomWalletButton />
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Container, Grid } from "@mantine/core";
+import { useTheme } from "@emotion/react";
+import { Container, Grid, ScrollArea } from "@mantine/core";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import Navbar from "../components/Navbar";
 import { useProgram } from "../hooks";
@@ -8,14 +9,144 @@ export default function Home() {
 
 	return (
 		<Container style={{ minWidth: "100%", overflow: "auto", height: "100vh" }} p={0} m={0}>
-			<Grid style={{ minHeight: "100vh", position: "relative" }} m={0}>
-				<Grid.Col xs={2} style={{ background: "#2c2f33" }}>
-					<Navbar />
+			<Grid style={{ minHeight: "100vh" }} m={0}>
+				<Grid.Col xs={2} style={{ background: "#2c2f33", position: "relative" }} p={0}>
+					<Container
+						style={{
+							position: "sticky",
+							top: 0,
+							margin: 0,
+							padding: 0,
+							height: "100vh",
+						}}>
+						<Navbar />
+					</Container>
 				</Grid.Col>
-				<Grid.Col xs={7} style={{ background: "#23272a" }} p={20}>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil provident quam minima ducimus
-					voluptate! Exercitationem accusamus natus aliquam ad ratione? Cupiditate perferendis officiis
-					nesciunt nam, velit quos voluptatem laborum possimus.
+				<Grid.Col xs={7} style={{ background: "#23272a" }}>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae perferendis
+					error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque cupiditate
+					saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem
+					molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat
+					doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+					Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos
+					magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur
+					adipisicing elit. Placeat nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed
+					fuga nemo quos magni qui repellat doloremque cupiditate saepe id quam? p=
+					{20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat nihil autem molestiae
+					perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui repellat doloremque
+					cupiditate saepe id quam? p={20}Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+					nihil autem molestiae perferendis error. Accusantium natus ad, numquam sed fuga nemo quos magni qui
+					repellat doloremque cupiditate saepe id quam? p={20}
 				</Grid.Col>
 				<Grid.Col xs={3} style={{ background: "#2c2f33" }}>
 					Recommendations
