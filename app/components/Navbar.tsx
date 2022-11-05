96cef24c942e833b919f952a2b1363d6e1429b8d
srijan mukherjee <emailofsrijan@gmail.com>
2022-11-05 22:06:43 +0530

    adding font awesome

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,6 +3,8 @@ import { FC, PropsWithChildren } from "react";
 
 import CustomWalletButton from "./CustomWalletButton";
 import WalletMultiButton from "./nossr/WalletMultiButton";
+import Link from "next/link";
+import NavbarLink from "./NavbarLink";
 
 interface NavbarProps extends PropsWithChildren {}
 
@@ -14,14 +16,15 @@ const useStyles = createStyles((theme) => ({
 	},
 }));
 
+const navbarLinks = [{ label: "Explore", icon: null }];
+
 const Navbar: FC<NavbarProps> = () => {
 	const { classes } = useStyles();
+	const links = navbarLinks.map((item) => <NavbarLink {...item} key={item.label} />);
 
 	return (
 		<MantineNavbar width={{ base: "100%" }} height="100%" style={{ background: "transparent", border: "none" }}>
-			<MantineNavbar.Section grow>
-				<div>Hello</div>
-			</MantineNavbar.Section>
+			<MantineNavbar.Section grow>{links}</MantineNavbar.Section>
 			<MantineNavbar.Section>
 				<CustomWalletButton />
 			</MantineNavbar.Section>
